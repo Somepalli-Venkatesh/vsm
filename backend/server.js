60f97796fc52cff64340b63f8ce654c09dc49fc0
Somepalli-Venkatesh <venkateshsomepalli999@gmail.com>
2025-02-18 00:48:00 +0530

    server file updated

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 // Configure Socket.io to accept any origin
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Allow requests only from this origin
+    origin: "https://vsm-virtual-study-front.vercel.app/", // Allow requests only from this origin
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -195,5 +195,5 @@ io.on("connection", (socket) => {
 });
 
 // Listen on port 5173 or environment variable PORT
-const PORT = process.env.PORT || 5173;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
