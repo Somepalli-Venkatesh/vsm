4762b97aea7b76d7d11ed2205f2ccef146e586a0
Somepalli-Venkatesh <venkateshsomepalli999@gmail.com>
2025-02-18 00:30:12 +0530

    chunks related

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // This function tells Rollup to split vendor code into its own chunk
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        },
+      },
+    },
+    // Increase the warning limit if necessary (value is in kB)
+    chunkSizeWarningLimit: 1000,
+  },
 })
